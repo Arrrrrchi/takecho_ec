694c8f1ee76ce1383ade3ecad6ccf9f261111ebf
Arrrrrchi <esh6c4nm@gmail.com>
2023-04-13 13:38:27 +0900

    認証エラー修正

--- routes/admin.php
+++ routes/admin.php
@@ -30,7 +30,7 @@ Route::get('/', function () {
 
 Route::get('/dashboard', function () {
     return view('admin.dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['auth:admin', 'verified'])->name('dashboard');
 
 Route::middleware('auth:admin')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
